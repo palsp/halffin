ea8bdeb357dd9c8921d98b88c005e1fb851f1dc0
palsp <palsp2406@gmail.com>
2021-11-27 15:51:25 +0700

    fix: deploy error

--- tests/escrows_factory/test_create_product.py
+++ tests/escrows_factory/test_create_product.py
@@ -1,5 +1,5 @@
 from brownie import network, Contract, Escrow
-from scripts.helpful_scripts import LOCAL_BLOCKCHAIN_ENVIRONMENTS, get_account, LOCK_PERIOD, SELLING_PRICE, PRODUCT_NAME, PRODUCT_URI
+from scripts.helpful_scripts import LOCAL_BLOCKCHAIN_ENVIRONMENTS, fund_with_link, get_account, LOCK_PERIOD, SELLING_PRICE, PRODUCT_NAME, PRODUCT_URI
 from scripts.escrow_factory.deploy_factory import deploy_factory
 from scripts.escrow_factory.create_product import create_product
 
@@ -11,6 +11,7 @@ def test_can_create_product():
         pytest.skip("for development only")
     account = get_account()
     escrow_factory = deploy_factory(LOCK_PERIOD)
+    fund_with_link(escrow_factory)
     tx = escrow_factory.createProduct(
         PRODUCT_NAME, SELLING_PRICE, PRODUCT_URI, LOCK_PERIOD + 10, {"from": account})
     product_address = tx.events["ProductCreated"]["product"]
